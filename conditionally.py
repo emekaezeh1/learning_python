09a2835c83a710b1a5aa5d65df561f2e795c4b99
Emeka Ezeh <emekaezeh5@gmail.com>
2024-06-24 16:05:05 -0500

    Another conitional script

---
+++ conditionally.py
@@ -0,0 +1,9 @@
+name = "Kevin"
+if len(name) >= 6:
+    print("name is long")
+elif len(name) == 5:
+    print("name is 5 characters")
+elif len(name) >= 4:
+    print("name is 4 or more")
+else:
+    print("name is short")
