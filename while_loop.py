d74c929bdd1babe938a4e2863235c35b5e7bf23b
Emeka Ezeh <emekaezeh5@gmail.com>
2024-06-24 16:32:10 -0500

    Modified While_loop

--- while_loop.py
+++ while_loop.py
@@ -10,3 +10,10 @@ while count < 10:
         continue
     print(f"We're counting odd numbers: {count}")
     count += 1
+    
+#while count < 10:
+    #if count % 2 == 0:
+      #  break
+   # print(f"We're counting odd numbers:{count}")
+    #count += 1
+
---
+++ while_loop2.py
@@ -0,0 +1,5 @@
+while count < 10:
+    if count % 2 == 0:
+        break
+    print(f"We're counting odd numbers:{count}")
+    count += 1
