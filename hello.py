37e71e0a4eba941e0cd569ec752065a6abe46170
Emeka Ezeh <emekaezeh5@gmail.com>
2024-06-24 16:15:16 -0500

    Added some python scripts

---
+++ hello.py
@@ -0,0 +1,19 @@
+count = 10
+while(count > 0):
+    print("*" * count)
+    count -= 1
+
+for i in range(1,11):
+    print("*" * i)
+myStr = "test"
+for c in myStr:
+    print(c)
+
+myList = ["Hey there!!!", "How is it going?", "More words", "small talk", "...."]
+for l in myList:
+    print(l)
+
+for i in range(0, 10):
+    if i < 8:
+        pass
+    print(i)
---
+++ while_loop.py
@@ -0,0 +1,12 @@
+count = 1
+while count <= 4:
+    print("looping")
+    count += 1
+
+count = 0
+while count < 10:
+    if count % 2 == 0:
+        count += 1
+        continue
+    print(f"We're counting odd numbers: {count}")
+    count += 1
