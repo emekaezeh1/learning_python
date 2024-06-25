712719e8984fefa68cc152afbc3bbb4075d98045
Emeka Ezeh <emekaezeh5@gmail.com>
2024-06-25 18:10:47 -0500

    basic scripting in python

---
+++ Basic_Scripting/function.py
@@ -0,0 +1,17 @@
+def hello_world():
+    print("Hello, World!")
+
+
+
+    def print_name(name):
+        print(f"Name is {name}")
+
+    print_name("Larry")
+
+    output = print_name(Keith)
+
+
+    def add_two(num):
+        return num + 2
+    result = add_two(2)
+    print(result)
